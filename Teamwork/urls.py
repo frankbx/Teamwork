76e0f746e008476bb72d37454e13b90009079bf5
Bao Xiang <xiang.bao.frank@gmail.com>
2017-01-06 21:35:03 +0800

    first template

--- Teamwork/urls.py
+++ Teamwork/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from EasyEquipment import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'EasyEquipment.views.home', name='home'),
+    url(r'^$', views.home_page, name='home'),
 ]
